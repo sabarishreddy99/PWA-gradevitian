598a7354b787bddecdac959026ca14f9fe740eec
sabarishreddy99 <64861603+sabarishreddy99@users.noreply.github.com>
2020-11-11 00:28:49 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-    console.log('ServiceWorker registration successful with scope:',  registration.scope);
-  }).catch(function(error) {
-    console.log('ServiceWorker registration failed:', error);
-  });
-}
-
