8a591df8630604f5ea34887822e9d90eab5db3f5
sabarishreddy99 <64861603+sabarishreddy99@users.noreply.github.com>
2020-11-10 11:05:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(function (registration) {
-      console.log('Registration successful, scope is:', registration.scope);
-    })
-    .catch(function (error) {
-      console.log('Service worker registration failed, error:', error);
-    });
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+    console.log('ServiceWorker registration successful with scope:',  registration.scope);
+  }).catch(function(error) {
+    console.log('ServiceWorker registration failed:', error);
+  });
 }
+
